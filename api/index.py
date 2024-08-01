ef4ffa26cd07a175a14e2e0a3da27c7ce1e92dc8
paliman01 <163369665+paliman01@users.noreply.github.com>
2024-08-01 20:44:58 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,15 @@
-from flask import Flask
+
+from flask import Flask, request
+import paho.mqtt.publish as publish
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return 'Hello, World!'
+def index():
+    mqtt_topic = request.args.get('mqtt_topic', '')
+    mqtt_payload = request.args.get('mqtt_payload', '')
+    publish.single(mqtt_topic, mqtt_payload, hostname="broker.mqttgo.io")
+    return f'MQTT Topic: {mqtt_topic}, MQTT Payload: {mqtt_payload}. Message sent.'
 
-@app.route('/about')
-def about():
-    return 'About'
+if __name__ == '__main__':
+    app.run(debug=True)
